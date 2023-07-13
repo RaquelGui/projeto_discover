2c48ed5b97ca8bac1654a0af1f7ac35908f00fdf
Raquel <raquelgoulart@gmail.com>
2023-07-13 23:03:27 +0100

    background corretion

--- .VSCODE/Script.js
+++ .VSCODE/Script.js
@@ -1,12 +1,12 @@
-function ToggleMode () {
+function toggleMode () {
   const html = document.documentElement
   html.classList.toggle("light")
 
-const img =document.querySelector('profile img')
-  if(html.classList.contains('light')) {
-img.setAttribute('src','./assets/avatar-light.png')
+const img = document.querySelector("profile img")
+  if (html.classList.contains("light")) {
+  img.setAttribute("src","./assets/avatar-light.png")
 } else {
-img.setAttribute('src','./assets/avatar.png')
+img.setAttribute("src","./assets/avatar.png")
 }
 
 
